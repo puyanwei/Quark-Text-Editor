87d3b0dd92b1acbf4c84d106acbeaffdadcf7579
Abi Travers <atravers4007@gmail.com>
2017-10-11 09:43:21 +0100

    Displaying the file in the electron app window

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-document.write('The current version of io.js' + process.version)
+// document.write('The current version of io.js' + process.version)
 
 var fs = require('fs')
+
+
+
+
 var content = fs.readFileSync('./file', 'utf8')
-alert(content)
+document.write(content)
