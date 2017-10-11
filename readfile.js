af36533138ec9930d06f30c4ea551c3431adcf4c
Frederick Ofori <FrederickCodes@gmail.com>
2017-10-11 18:11:46 +0100

    test added for load button

--- readfile.js
+++ readfile.js
@@ -1,12 +1,34 @@
+
 const fs = require('fs')
 
 window.onload = function (){
 
+  var btn = document.getElementById("loadbutton");
 
-var editor = document.getElementById('editor');
+  btn.addEventListener("click", loadFile);
 
-var readThisFile = fs.readFileSync('file.txt', 'utf8');
+  function loadFile() {
+    var editor = document.getElementById('editor');
 
-editor.innerText = readThisFile;
+    var readThisFile = fs.readFileSync('file.txt', 'utf8');
 
-};
+    editor.innerText = readThisFile;
+  }
+
+}
+// module.exports = loadFile
+
+
+// window.onload = function (){
+//
+//   var btn = document.getElementById("btn");
+//   // var notes = []
+//
+//   btn.addEventListener("click", function(){
+//     var htmlNote = document.getElementById("noteForm").value;
+//     noteList.addNote(htmlNote);
+//     controller.changeView();
+//     // notes.push(newNote)
+//   })
+//
+// }
