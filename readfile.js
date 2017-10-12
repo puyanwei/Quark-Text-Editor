324247239efee45a9e48ecec57b9ada6f1441d2b
Frederick Ofori <FrederickCodes@gmail.com>
2017-10-12 12:05:09 +0100

    Save feature added so the user can now save any text inputted to a file and directory of their choice

--- readfile.js
+++ readfile.js
@@ -1,11 +1,20 @@
-
+const {dialog} = require('electron').remote;
 const fs = require('fs')
+
 
 window.onload = function (){
 
-  var btn = document.getElementById("loadbutton");
+  // Buttons
 
-  btn.addEventListener("click", loadFile);
+  var saveBtn = document.getElementById("savebutton");
+
+  saveBtn.addEventListener("click", saveFile);
+
+  var loadBtn = document.getElementById("loadbutton");
+
+  loadBtn.addEventListener("click", loadFile);
+
+ //  Load
 
   function loadFile() {
     var editor = document.getElementById('editor');
@@ -14,6 +23,26 @@ window.onload = function (){
 
     editor.innerText = readThisFile;
   }
+
+
+  //  save
+  function saveFile(){
+    let content = document.getElementById('editor').innerText
+
+    dialog.showSaveDialog((filename) => {
+      if (filename === undefined){
+        console.log("You didn't save the file");
+        return;
+      }
+
+      fs.writeFile(filename, content, (err) => {
+        if (err){
+          alert("An error occured creating the file " + err.message)
+        }
+        alert("The file has been succesfully saved");
+      });
+    });
+  };
 
 }
 // module.exports = loadFile
---
+++ test/writeFileTest.js
@@ -0,0 +1,35 @@
+const electron = require("electron");
+var expect = require("chai").expect;
+
+var Application = require("spectron").Application;
+var assert = require("assert");
+
+describe("application launch", function() {
+  this.timeout(10000);
+
+  beforeEach(function() {
+    this.app = new Application({
+      path: `${__dirname}/../node_modules/.bin/electron`,
+      args: ["main.js"]
+    });
+    return this.app.start();
+  });
+
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it("should save text our editor into a file", function(){
+    return this.app.client
+      .waitUntilWindowLoaded()
+      // .leftClick('#savebutton')
+      // // save filename
+      // .leftClick('#loadbutton')
+      // // load the filename you have saved after
+      // .getText('#editor')
+      // // check that the text in editor still match
+      // .then(text => expect(text).to.eq('This is a test.'))
+  })
+});
