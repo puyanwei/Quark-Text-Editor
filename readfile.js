a8e95dd32634dc7bb352846e9d56ea0d53e24389
pyan83 <puyanwei@gmail.com>
2017-10-13 19:11:36 +0100

    got a word count to work using the button

--- readfile.js
+++ readfile.js
@@ -1,12 +1,9 @@
-const {dialog} = require('electron').remote;
-const fs = require('fs')
+const { dialog } = require("electron").remote;
+const fs = require("fs");
 
-
-
-window.onload = function (){
-
-  let currentFileName = "nothing"
-  console.log(currentFileName)
+window.onload = function() {
+  let currentFileName = "nothing";
+  console.log(currentFileName);
 
   // Buttons
 
@@ -22,76 +19,80 @@ window.onload = function (){
 
   saveBtn.addEventListener("click", saveFile);
 
- //  Load
+  // function initPage() {
+  //   var elEd = document.getElementById("editor");
+  //   elEd.contentEditable = true;
+  //   elEd.focus();
+  // }
+  // initPage();
+
+  var wordCountBtn = document.getElementById("word-count");
+
+  wordCountBtn.addEventListener("click", wordCount);
+
+  function wordCount() {
+    var text = document.getElementById("editor").innerText;
+    console.log(text.split(" ").length);
+  }
+
+  //  Load
 
   function loadFile() {
+    var editor = document.getElementById("editor");
 
-    var editor = document.getElementById('editor');
-
-    dialog.showOpenDialog((fileNames) => {
-
-
+    dialog.showOpenDialog(fileNames => {
       // if (fileNames[0] === undefined){
       //   console.log("No files were selected");
       //   return;
       // }
 
-      currentFileName = fileNames[0]
-        console.log(currentFileName)
+      currentFileName = fileNames[0];
+      console.log(currentFileName);
 
-       fs.readFile(fileNames[0], 'utf8', (err, data) => {
-         if(err){
-           return alert("An arror occured reading the file :");
-         }
-         console.log("the content of the file is: " + data);
-         editor.innerText =  data
-
-       })
-
-
-    })
+      fs.readFile(fileNames[0], "utf8", (err, data) => {
+        if (err) {
+          return alert("An arror occured reading the file :");
+        }
+        console.log("the content of the file is: " + data);
+        editor.innerText = data;
+      });
+    });
   }
 
-
   //  save
-  function saveAsFile(){
-    let content = document.getElementById('editor').innerText
+  function saveAsFile() {
+    let content = document.getElementById("editor").innerText;
 
-    dialog.showSaveDialog((fileName) => {
-      if (filename === undefined){
+    dialog.showSaveDialog(fileName => {
+      if (filename === undefined) {
         console.log("You didn't save the file");
         return;
       }
 
-      currentFileName = fileName
+      currentFileName = fileName;
 
-      fs.writeFile(fileName, content, (err) => {
-        if (err){
-          alert("An error occured creating the file " + err.message)
+      fs.writeFile(fileName, content, err => {
+        if (err) {
+          alert("An error occured creating the file " + err.message);
         }
         alert("The file has been succesfully saved");
       });
-
-
-    });
-  };
-
-  function saveFile(){
-
-    let content = document.getElementById('editor').innerText
-
-    fs.writeFile(currentFileName , content, (err) => {
-      if (err){
-        alert("An error occured creating the file " + err.message)
-      }
-      alert("The file has been succesfully saved");
-      console.log(currentFileName)
     });
   }
 
-}
-// module.exports = loadFile
+  function saveFile() {
+    let content = document.getElementById("editor").innerText;
 
+    fs.writeFile(currentFileName, content, err => {
+      if (err) {
+        alert("An error occured creating the file " + err.message);
+      }
+      alert("The file has been succesfully saved");
+      console.log(currentFileName);
+    });
+  }
+};
+// module.exports = loadFile
 
 // window.onload = function (){
 //
