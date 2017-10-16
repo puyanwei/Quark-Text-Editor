267964c254880d2cd86d21fbd9c19a54a4d5e78d
Frederick Ofori <FrederickCodes@gmail.com>
2017-10-16 16:48:11 +0100

    Fixed typo in saveAsFile function

--- readfile.js
+++ readfile.js
@@ -1,7 +1,12 @@
-const { dialog } = require("electron").remote;
-const fs = require("fs");
+const electron = require('electron');
+const {dialog} = require('electron').remote;
+const fs = require('fs')
+const ipc = electron.ipcRenderer
 
-window.onload = function() {
+
+
+window.onload = function (){
+
   let currentFileName;
 
   // Buttons
@@ -18,73 +23,76 @@ window.onload = function() {
 
   saveBtn.addEventListener("click", saveFile);
 
-  //  Load
-  var wordCountBtn = document.getElementById("word-count");
-
-  wordCountBtn.addEventListener("click", wordCount);
-
-  function wordCount() {
-    var text = document.getElementById("editor").innerText;
-    console.log(text.split(" ").length);
-  }
+ //  Load
 
   function loadFile() {
-    var editor = document.getElementById("editor");
 
-    dialog.showOpenDialog(fileNames => {
+    var editor = document.getElementById('editor');
+
+    dialog.showOpenDialog((fileNames) => {
+
+
       // if (fileNames[0] === undefined){
       //   console.log("No files were selected");
       //   return;
       // }
 
-      currentFileName = fileNames[0];
-      console.log(currentFileName);
+      currentFileName = fileNames[0]
+        console.log(currentFileName)
 
-      fs.readFile(fileNames[0], "utf8", (err, data) => {
-        if (err) {
-          return alert("An arror occured reading the file :");
-        }
-        console.log("the content of the file is: " + data);
-        editor.innerText = data;
-        updateSyntax();
-      });
-    });
+       fs.readFile(fileNames[0], 'utf8', (err, data) => {
+         if(err){
+           return alert("An arror occured reading the file :");
+         }
+         console.log("the content of the file is: " + data);
+         editor.innerText =  data
+         updateSyntax()
+       })
+
+
+    })
   }
 
-  //  save
-  function saveAsFile() {
-    let content = document.getElementById("editor").innerText;
 
-    dialog.showSaveDialog(fileName => {
-      if (filename === undefined) {
+  //  save
+  function saveAsFile(){
+    let content = document.getElementById('editor').innerText
+
+    dialog.showSaveDialog((fileName) => {
+      if (fileName === undefined){
         console.log("You didn't save the file");
         return;
       }
 
-      currentFileName = fileName;
+      currentFileName = fileName
 
-      fs.writeFile(fileName, content, err => {
-        if (err) {
-          alert("An error occured creating the file " + err.message);
+      fs.writeFile(fileName, content, (err) => {
+        if (err){
+          alert("An error occured creating the file " + err.message)
         }
         alert("The file has been succesfully saved");
       });
+
+
     });
-  }
+  };
 
-  function saveFile() {
-    let content = document.getElementById("editor").innerText;
+  function saveFile(){
 
-    fs.writeFile(currentFileName, content, err => {
-      if (err) {
-        alert("An error occured creating the file " + err.message);
+    let content = document.getElementById('editor').innerText
+
+    fs.writeFile(currentFileName , content, (err) => {
+      if (err){
+        alert("An error occured creating the file " + err.message)
       }
       alert("The file has been succesfully saved");
-      console.log(currentFileName);
+      console.log(currentFileName)
     });
   }
-};
+
+}
 // module.exports = loadFile
+
 
 // window.onload = function (){
 //
