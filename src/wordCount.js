4eb4378c851235040ffdec710f9ea33a4dc2398c
pyan83 <puyanwei@gmail.com>
2017-10-18 20:05:02 +0100

    added bootstrap setup, put buttons in header

--- src/wordCount.js
+++ src/wordCount.js
@@ -1,9 +1,25 @@
 // let wordCountBtn = document.getElementById("word-count");
+document.addEventListener("keydown", characterCount);
 document.addEventListener("keydown", wordCount);
+document.addEventListener("keydown", lineCount);
+
+function characterCount() {
+  let text = document.getElementById("editor").innerText;
+  let words = text.split(/\s+/g).length;
+
+  document.getElementById("character-counter").innerText = words;
+}
 
 function wordCount() {
   let text = document.getElementById("editor").innerText;
   let words = text.split(/\s+/g).length;
 
-  document.getElementById("counter").innerText = words;
+  document.getElementById("word-counter").innerText = words;
+}
+
+function lineCount() {
+  let text = document.getElementById("editor").innerText;
+  let words = text.split(/\s+/g).length;
+
+  document.getElementById("line-counter").innerText = words;
 }
