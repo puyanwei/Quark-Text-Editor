a8490bc153133f01475dc004ab0ea97b125cebc8
David Divilly <d.divilly@gmail.com>
2017-10-19 17:03:09 +0100

    Added current file to saveAsFile

--- assets/renderer.js
+++ assets/renderer.js
@@ -5,17 +5,23 @@ const mainProcess = remote.require('./main')
 const selectedEditor = document.querySelector('#editor')
 
 
-ipc.on('open-file', (event, file, content) => {
+ipc.on('open-file', (event, fileName, content) => {
   selectedEditor.innerHTML = content
+  currentFileName = fileName
 });
 
 ipc.on('save-as-file', (event) => {
-  const content = selectedEditor.innerHTML
+  const content = selectedEditor.innerText
   mainProcess.saveAsFile(content)
 });
 
+ipc.on('file-name', (event, fileName) => {
+  currentFileName = fileName
+  console.log(currentFileName)
+})
+
 ipc.on('save-file', (event) => {
-  const saveContent = selectedEditor.innerHTML
+  const saveContent = selectedEditor.innerText
   mainProcess.saveFile(saveContent)
   alert("Wooooo your file was saved")
 })
